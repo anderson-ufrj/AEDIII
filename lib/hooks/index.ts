8b2b509e39f915b97b42a292230ec571b844df1f
anderson-ufrj <anderson-ufrj@users.noreply.github.com>
2025-11-18 16:09:11 -0300

    refactor(hooks): centralizar exports de hooks customizados
    Cria barrel export para todos os hooks:
    - useKeyboardNavigation
    - useAtalhosTeclado
    - useProgressoLeitura
    - useCachedData
    - useMobile
    
    Simplifica imports: import { useX, useY } from '@/lib/hooks'

---
+++ lib/hooks/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Barrel export de todos os hooks customizados
+ * Facilita importação centralizada
+ */
+
+export { useKeyboardNavigation } from './use-keyboard-navigation';
+export { useAtalhosTeclado } from './use-atalhos-teclado';
+export { useProgressoLeitura, obterTodosProgressos } from './use-progresso-leitura';
+export { useCachedData } from '../utils/cache-manager';
+
+// Re-export de hooks do diretório hooks/
+export { useMobile } from '../../hooks/use-mobile';
